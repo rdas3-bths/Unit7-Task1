e85c823db53e4c4e7c13f31057a95fce841443d3
RDas3 <das@>
2024-02-01 09:56:28 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // Sample input answer part one: 14
+        // sample input answer part two: 3
+        // Test input answer part one: 612
+        // Test input answer part two: 83
+        ArrayList<String> fileData = getFileData("data/brick_layout");
+        ArrayList<Brick> bricks = new ArrayList<Brick>();
+        for (String line : fileData) {
+            String[] points = line.split(",");
+            int start = Integer.parseInt(points[0]);
+            int end = Integer.parseInt(points[1]);
+            Brick b = new Brick(start, end);
+            bricks.add(b);
+        }
+
+
+        System.out.println(partOne(bricks));
+        System.out.println(partTwo(bricks));
+    }
+
+    private static int partOne(ArrayList<Brick> bricks) {
+        // Implement me!
+        return 0;
+    }
+
+    private static int partTwo(ArrayList<Brick> bricks) {
+        // Implement me!
+        return 0;
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        File f = new File(fileName);
+        Scanner s = null;
+        try {
+            s = new Scanner(f);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(1);
+        }
+        ArrayList<String> fileData = new ArrayList<String>();
+        while (s.hasNextLine())
+            fileData.add(s.nextLine());
+
+        return fileData;
+    }
+}
